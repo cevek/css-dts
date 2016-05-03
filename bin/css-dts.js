52278ed16ebe2035840989551320c918e19c6a21
cody <cevek@ya.ru>
2016-05-03 12:41:02 +0500

    Add camelCase pipe for class names

--- bin/css-dts.js
+++ bin/css-dts.js
@@ -3,6 +3,7 @@
 
 var fs = require('fs');
 var processCss = require('css-loader/lib/processCss.js');
+var camelCase = require("lodash.camelcase");
 var options = {
     query: {
         root: {},
@@ -14,15 +15,19 @@ var options = {
 };
 
 var file = process.argv[2];
-if (file.match(/\.css$/)) {
+if (file && file.match(/\.css$/)) {
     var fileData = fs.readFileSync(file).toString();
     var filedTs = file + '.d.ts';
+
     processCss(fileData, null, options, (err, result)=> {
         if (err) {
+            console.error(err.stack);
             throw err;
         }
-        const keys = Object.keys(result.exports);
+        const keys = Object.keys(result.exports).map(camelCase);
         const newData = `export const locals: {\n\t${keys.map(key => `${key}: string`).join('\n\t')}\n}`;
         fs.writeFileSync(filedTs, newData);
     });
+} else {
+    throw new Error(`Incorrect input file: ${file}`);
 }
